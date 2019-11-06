83345b07c0574d75653e87d39d3a7410af382bd3
Karthi <karthi.anandhan@freshworks.com>
2019-11-06 20:45:52 +0530

    basic tableview setup

--- CardSwipe/ViewController.swift
+++ CardSwipe/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardsTableview: UITableView!
+    
+      var tableData: [String] = ["Christ Redeemer", "Great Wall of China", "Machu Picchu","Petra","Pyramid at Chichén Itzá","Roman Colosseum","Taj Mahal"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpTablview()
     }
-
-
+    
+    func setUpTablview()  {
+        self.cardsTableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.cardsTableview.delegate = self
+        self.cardsTableview.dataSource = self
+        self.cardsTableview.allowsSelection = false
+    }
 }
+
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.cardsTableview.dequeueReusableCell(withIdentifier: "cell")!
+
+        cell.textLabel?.text = self.tableData[indexPath.row]
+
+        return cell
+    }
+}
+
 
