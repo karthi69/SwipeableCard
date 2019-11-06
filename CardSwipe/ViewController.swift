534d2fd88e4740748f63086afb66144650b3c87a
Karthi <karthi.anandhan@freshworks.com>
2019-11-06 20:25:26 +0530

    Initial Commit

---
+++ CardSwipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardSwipe
+//
+//  Created by Karthi Anandhan on 06/11/19.
+//  Copyright © 2019 Karthi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
